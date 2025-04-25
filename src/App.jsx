f776abf60764621f58cf92137bb0a1f1cd749f6e
missinggoverner <zekariyasamdu24@gmail.com>
2025-04-25 21:40:47 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Adder from './components/adder'
+export default  function App() {
+  
+
+  return (
+    <>
+        <Adder/>
+    </>
+  )
+}
+
+
---
+++ src/components/adder.jsx
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+export default function Adder() {
+    return(
+        <div>
+            <h1>hello</h1>            
+        </div>
+    )
+}
